6886f83cd514aaed437dd442537d0c7b3267f7a3
wutong <1936739653@qq.com>
2024-08-12 19:35:39 +0800

    MKITest

--- tests/unittest/api/kernel_test.cpp
+++ tests/unittest/api/kernel_test.cpp
@@ -3,7 +3,7 @@
  * MindKernelInfra is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
- *           http://license.coscl.org.cn/MulanPSL2
+ *             http://license.coscl.org.cn/MulanPSL2
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
  * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
@@ -17,23 +17,6 @@
 
 namespace {
 	using namespace Mki;
-
-	class OperationNew : public OperationBase{
-		public:
-			OperationNew() : OperationBase("") {}
-			std::string GetName() const override { return ""; };
-			Mki::Status InferShape(LaunchParam &launchParam) const override { return Status::OkStatus(); };
-			int64_t GetInputNum(const Any &specificParam) const override { return 1; };
-			int64_t GetOutputNum(const Any &specificParam) const override { return 1; };
-			Kernel *GetBestKernel(const LaunchParam &launchParam) const override { return nullptr; }
-
-		protected:
-			Status InferShapeImpl(const LaunchParam &launchParam, SVector<Tensor> &outTensors) const override
-			{
-				outTensors[0].desc = launchParam.GetOutTensor(0).desc;
-				return Status::OkStatus();
-			}
-	};
 
 	class KernelNew : public Kernel{
 		public:
@@ -53,58 +36,24 @@ namespace {
 			std::string GetName() const override { return kernelName_; };
 			const KernelInfo &GetKernelInfo() const override {return kernelInfo_;};
 			KernelType GetType() const override { return Mki::KERNEL_TYPE_AI_CORE; };
+
 		private:
 			Mki::KernelInfo kernelInfo_;
 			std::string kernelName_;
 	};
 
-	TEST(OperationBaseTest, all)
-	{
-		Mki::Operation *op = new OperationNew();
-		std::unique_ptr<Kernel> kernel{nullptr};
+	TEST(KernelBaseTest,all){
 		LaunchParam launchParam;
 		Tensor inTensor;
-		Tensor outTensor = inTensor;
+		Tensor outTensor=inTensor;
 		launchParam.AddInTensor(inTensor);
 		launchParam.AddOutTensor(outTensor);
+		Mki::KernelInfo kernelInfo;
 
-		kernel.reset(op->GetBestKernel(launchParam));
-		EXPECT_EQ(kernel, nullptr);
-		EXPECT_EQ(op->IsConsistent(launchParam), true);
-		delete op;
-		op = nullptr;
+		Mki::Kernel *kernel = new KernelNew("Test");
+		EXPECT_EQ(kernel->CanSupport(launchParam),true);
+		EXPECT_EQ(kernel->GetTilingSize(launchParam),1);
+		EXPECT_EQ(kernel->GetName(), "Test");
+		EXPECT_EQ(kernel->GetType(), Mki::KERNEL_TYPE_AI_CORE);
 	}
-
-	TEST(OperationBaseTest, all0)
-	{
-		Mki::Operation *op = new OperationNew();
-		const KernelList &kernelList = op->GetKernelList();
-		EXPECT_EQ(kernelList.size(), 0);
-		delete op;
-		op = nullptr;
-	}
-
-	TEST(OperationBaseTest, all1)
-	{
-		Mki::Operation *op = new OperationNew();
-		std::unique_ptr<Kernel> kernel{nullptr};
-		kernel.reset(op->GetKernelByName("Test"));
-		EXPECT_EQ(kernel, nullptr);
-		delete op;
-		op = nullptr;
-	}
-
-	TEST(OperationBaseTest, all2)
-	{
-		Mki::OperationBase *op = new OperationNew();
-		std::string kernelName = "Test";
-		Mki::Kernel *kernel = new KernelNew(kernelName);
-		op->AddKernel(kernelName, kernel);
-		EXPECT_EQ(op->GetKernelByName(kernelName)->GetName(), "Test");
-		delete op;
-		op = nullptr;
-	}
-
-
-} // namespace
-
+}// namespace
