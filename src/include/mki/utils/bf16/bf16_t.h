a949d0df169545e291ac169ea057b07bc1c13f8f
徐天昊 <xutianhao3@h-partners.com>
2025-01-15 16:24:20 +0800

    cleancode

--- src/include/mki/utils/bf16/bf16_t.h
+++ src/include/mki/utils/bf16/bf16_t.h
@@ -7,6 +7,7 @@
  * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
  * See LICENSE in the root of the software repository for the full text of the License.
  */
+
 #ifndef MKI_UTILS_BF_16_BF_16_T_H
 #define MKI_UTILS_BF_16_BF_16_T_H
 
