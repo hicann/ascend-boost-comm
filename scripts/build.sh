a053aa35e6fc73761135761fa655e89b4fd76158
Hall <qinhao61@h-partners.com>
2025-05-08 17:08:23 +0000

    update scripts/build.sh.
    Signed-off-by: Hall <qinhao61@h-partners.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -134,7 +134,6 @@ function fn_install_cann_and_kernel()
     ln -s ${ASCEND_HOME_PATH}/compiler/$(arch)-linux/ascendc/include/highlevel_api/               ${ASCEND_HOME_PATH}/$(arch)-linux/ascendc/include/ # platform_ascendc.h
     ln -s ${ASCEND_HOME_PATH}/compiler/include/acl/acl_op_compiler.h                              ${ASCEND_HOME_PATH}/$(arch)-linux/include/acl/ # acl_op_compiler.h
     cp -rf /usr1/CANN/mstx ${ASCEND_HOME_PATH}/tools/
-    cp -rf /usr1/CANN/mstx ${ASCEND_HOME_PATH}/include/
     cp -rf /usr1/CANN/mstx/include/mstx ${ASCEND_HOME_PATH}/include/
     ## lib64
     # mki
