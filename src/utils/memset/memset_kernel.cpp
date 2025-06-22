4b217ea36cdd92cd6dea66f4efbf3d109b8de4a2
caixilong <2508418876@qq.com>
2025-06-22 16:55:52 +0800

    fix bug

--- src/utils/memset/memset_kernel.cpp
+++ src/utils/memset/memset_kernel.cpp
@@ -136,7 +136,7 @@ public:
 
     Status BuildMemsetArgs(void **args, uint64_t argsNum, const MiniVector<KernelInfo::MemsetInfo> &memsetInfo, MkiRtKernelParam &kernelParam) const
     {
-        MemsetArgs memsetArgs;
+        static MemsetArgs memsetArgs;
         int ret = memset_s(&memsetArgs, sizeof(MemsetArgs), 0, sizeof(MemsetArgs));
         MKI_CHECK(ret == EOK, "memset_s memsetArgs Error! Error Code: " << ret, return Status::FailStatus(-1));
         for (size_t i = 0; i < MEMSET_MAX_TENSOR_NUM && i < memsetInfo.size() && i < argsNum; ++i) {
@@ -146,7 +146,7 @@ public:
         uint32_t blockDim = MemsetTiling(memsetInfo, memsetArgs.tilingData);
         MKI_CHECK(blockDim > 0, "failed to run memset tiling", return Status::FailStatus(1));
 
-        RtArgsExT argsEx;
+        static RtArgsExT argsEx;
         ret = memset_s(&argsEx, sizeof(RtArgsExT), 0, sizeof(RtArgsExT));
         MKI_CHECK(ret == EOK, "memset_s argsEx Error! Error Code: " << ret, return Status::FailStatus(-1));
 
